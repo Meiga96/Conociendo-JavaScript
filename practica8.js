b803907e306f592b9da0e0159bdfede758ef5196
Meiga96 <laura.fernandez@nortempoformacion.net>
2026-02-03 11:23:44 +0100

    Conociendo JavaScript

---
+++ practica8.js
@@ -0,0 +1,9 @@
+
+ let totalcompra= 60;
+    if (totalcompra >= 50) {
+        totalcompra = totalcompra - (totalcompra * 0.10);
+        console.log ("El total con descuento es: " + totalcompra);
+    } else {
+        console.log ("El total (sin descuento) es: " + totalcompra);
+    }
+
